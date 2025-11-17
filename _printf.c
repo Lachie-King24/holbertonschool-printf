1c5402af440336d1df445bac5f7b372fcb1329a7
Lachie King <kingy00112233@gmail.com>
2025-11-17 03:40:59 +0000

    update printf

--- _printf.c
+++ _printf.c
@@ -6,10 +6,7 @@
  * Return: Number of characters printed, exc null byte
  */
 
-
-
-/**
- * int _printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
 	va_list args;
 	int count;
@@ -61,56 +58,4 @@
 		va_end(args);
 		return (total);
 	}
-}
-*/
-
-int _printf(const char *format, ...)
-{
-    va_list args;
-    int count = 0, count_dir, total = 0, match;
-    dir dir[] = {
-        {"c", count_char},
-        {"s", count_string},
-        {"%", count_percent},
-        {NULL, NULL}
-    };
-
-    if (!format)  /* Safe: do nothing if format is NULL */
-        return 0;
-
-    va_start(args, format);
-
-    while (format[count] != '\0')
-    {
-        if (format[count] == '%')
-        {
-            match = 0;
-            for (count_dir = 0; dir[count_dir].symbol != NULL; count_dir++)
-            {
-                if (format[count + 1] == dir[count_dir].symbol[0])
-                {
-                    total += dir[count_dir].print(args);
-                    match = 1;
-                    break;
-                }
-            }
-            if (match)
-                count += 2;  /* Skip the specifier */
-            else
-            {
-                write(1, &format[count], 1);  /* Print unknown % literally */
-                total++;
-                count++;
-            }
-        }
-        else
-        {
-            write(1, &format[count], 1);  /* Print regular char */
-            total++;
-            count++;
-        }
-    }
-
-    va_end(args);
-    return total;
 }
